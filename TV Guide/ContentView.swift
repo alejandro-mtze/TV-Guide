dcc3f75b41db28552f4cd351411bb30e80d9673d
alejandro-mtze <alejandro.martinez.e89@gmail.com>
2021-05-20 16:44:59 -0500

    Initial Commit

---
+++ TV Guide/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TV Guide
+//
+//  Created by Blue Parrot Software Mac 01 on 5/20/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
